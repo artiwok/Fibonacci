6915ab4d2871865924ebcfa3a3faf4d9854a0773
artiwok <artemolga@ukr.net>
2020-11-12 01:54:44 +0200

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static int FibonacciPositive(int num)
+        {
+            if (num == 0)
+                return 0;
+
+            if (num == 1)
+                return 1;
+
+            return FibonacciPositive(num - 1) + FibonacciPositive(num - 2);
+        }
+
+        static int FibonacciNegative(int num)
+        {
+            if (num == 0)
+                return 0;
+
+            if (num == 1)
+                return 1;
+
+            return FibonacciNegative(num + 2) - FibonacciNegative(num + 1);
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"\nThe positive Fibonacci number = {FibonacciPositive(9)}");
+
+            Console.WriteLine($"\nThe negative Fibonacci number = {FibonacciNegative(-12)}\n");
+
+
+        }
+    }
+}
